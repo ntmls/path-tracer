9854ff38ac2d26d9d6664763b10979ed1f8831a4
Nathan Miles <ntmls@live.com>
2024-01-07 06:16:07 -0600

    Performance optimization

--- src/domain/common/bounds/SphereBounds.ts
+++ src/domain/common/bounds/SphereBounds.ts
@@ -4,17 +4,30 @@ import { Bounds } from "./Bounds";
 
 export class SphereBounds implements Bounds {
   private _radiusSquared: number;
+  private _centerX: number;
+  private _centerY: number;
+  private _centerZ: number;
 
-  constructor(readonly center: Vector, readonly radius: number) {
+  constructor(readonly center: Vector, radius: number) {
     this._radiusSquared = radius * radius;
+    this._centerX = center.x;
+    this._centerY = center.y;
+    this._centerZ = center.z;
   }
   inBounds(ray: Ray): boolean {
     const rayOrigin = ray.origin;
-    const rayDirection = ray.direction;;
-    
-    // project the vector to the center onto the ray
-    const time = this.center.minus(rayOrigin).dot(rayDirection);
-    const projected = rayOrigin.add(rayDirection.scale(time));
+    const rayDirection = ray.direction;
+
+    const time =
+      (this._centerX - rayOrigin.x) * rayDirection.x +
+      (this._centerY - rayOrigin.y) * rayDirection.y +
+      (this._centerZ - rayOrigin.z) * rayDirection.z;
+
+    const projected = new Vector(
+      rayDirection.x * time + rayOrigin.x,
+      rayDirection.y * time + rayOrigin.y,
+      rayDirection.z * time + rayOrigin.z
+    );
 
     const distanceSquared = projected.distanceSquaredFrom(this.center);
     if (distanceSquared > this._radiusSquared) {
@@ -27,7 +40,7 @@ export class SphereBounds implements Bounds {
         // the center of the intersection is behind the origion. We need to check if an actual intersection could be in frond of the origin.
         const halfIntersectionLengthSqr = this._radiusSquared - distanceSquared;
         const distFromOrigionToProjectedSqr =
-          ray.origin.distanceSquaredFrom(projected);
+          rayOrigin.distanceSquaredFrom(projected);
         if (distFromOrigionToProjectedSqr > halfIntersectionLengthSqr) {
           return true; // an intersection could be in front of the origion.
         } else {
